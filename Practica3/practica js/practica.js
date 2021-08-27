be2fdd0f0de7939d1c578ab848ea7ac94883679c
morenojessi <morenojessi43@gmail.com>
2021-08-27 00:56:48 -0400

    update

--- Practica3/practica js/practica.js
+++ Practica3/practica js/practica.js
@@ -1,3 +1,4 @@
+//Ejercicio 1
 function Multiplicar()
 {
     var num1 =parseFloat(document.getElementById('num1').value);
@@ -9,25 +10,27 @@ function Multiplicar()
     document.getElementById('res').value = resul
 }
 
+//Ejercicio 2
 function temperatura(){
     var temp =parseFloat(document.getElementById('temperatura').value);
-    if (temp >=-10){
-        if (temp<=15){
-            document.write('Clima: Frio') 
-        }
-    if (temp >=16){
-        if (temp <=25){
-            document.write('Clima: Templado')
-    if (temp >=26){
-        if (temp <=40){
-            document.write('Clima: Calor')
-        }
+    if (temp >=-10 && temp<=15){
+        var res="Frio";
     }
+        else 
+            if (temp >=16 && temp <=25){
+                res = "Templado";
+            }
+            else
+                if (temp >=26 && temp <=40){
+                    res = "Calor";
+                }
+                else
+                res= "valor inválido";
+    document.getElementById('clima'). value =res
 }
-}
-}
-}
+        
 
+//Ejercicio 3
 function factorial () {
     var valor = parseFloat(document.getElementById('valor').value);
 	var total = 1; 
@@ -35,10 +38,19 @@ function factorial () {
     {
 		total = total * i; 
 	}
-    document.getElementById('total').value = total;
+    document.getElementById('total').value = total
 }
 
+//Ejercicio 4
 function fecha(){
     var fecha = new Date();
-    document.write('Fechas Actual: '+ fecha.getDay +', de ' + fecha.getMonth + ' del ' + fecha.getFullYear)
+    var dias= ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+    var dia= dias[fecha.getDay()]
+    var meses= ['Enero', 'Febrero','Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    var mes = meses[fecha.getMonth()]
+    var year = fecha.getFullYear();
+    var fech = fecha.getDate();
+
+    document.getElementById('fecha1').value = dia
+    document.getElementById('fecha2').value = fech + ' de ' + mes + ' del ' + year
 }
