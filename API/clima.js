97746e22aa5301c4e036a71f6c4ba55a9f3007cb
morenojessi <morenojessi43@gmail.com>
2021-08-31 22:56:46 -0400

    update

--- API/clima.js
+++ API/clima.js
@@ -1,6 +1,6 @@
+//New York City: 5128581
+//APIKey: 186e8bc65d8fceee5afb57a6e9d69842
 //URL 
-//New York City 5128581
-// API KEY 186e8bc65d8fceee5afb57a6e9d69842
 
 const requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=5128581&units=metric&appid=186e8bc65d8fceee5afb57a6e9d69842';
 const request = new XMLHttpRequest();
@@ -9,26 +9,26 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-request.onlad = function() {
+request.onload = function() {
     const clima_json = request.response;
     console.log(clima_json);
-    MostrarInformacion(clima_json);
+    MostrarClima(clima_json);
 }
 
-function MostrarInformacion(climjson) {
+function MostrarClima(climjson) {
         
     var header = document.querySelector('header');
-    var h1 = document.querySelector('h1');
-    var temp = document.querySelector('h2');
-    var tem_max = document.querySelector('h2');
-    var tem_min = document.querySelector('h2');
-    var hum = document.querySelector('h2');
+    var h1 = document.createElement('h1');
+    var temp = document.createElement('h2');
+    var temp_max = document.createElement('h2');
+    var temp_min = document.createElement('h2');
+    var humidity = document.createElement('h2');
 
     h1.textContent = climjson.name;
-    temp.textContent = 'Temperatura Actual: ' + climjson.main.temp;
-    tem_max.textContent = 'Temperatura Máxima: ' + climjson.main.temp_max;
-    tem_min.textContent = 'Temperatura Minima: ' + climjson.main.temp_min;
-    hum.textContent = 'Humedad Relativa: ' + climjson.main.humidity;
+    temp.textContent = 'Temperatura Actual: ' + climjson.main.temp + ' °C';
+    temp_max.textContent = 'Temperatura Máxima: ' + climjson.main.temp_max + ' °C';
+    temp_min.textContent = 'Temperatura Minima: ' + climjson.main.temp_min + ' °C';
+    humidity.textContent = 'Humedad Relativa: ' + climjson.main.humidity + '%';
 
     header.appendChild(h1);
     header.appendChild(temp);
